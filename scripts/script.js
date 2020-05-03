e04138f107bb3600af4b65e98d88f4b1dd5371ad
André Arruda <33252657+andre23arruda@users.noreply.github.com>
2020-05-03 16:13:53 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,20 @@
+var t1 = null
+var t2 = null
+
+var audio = new Audio('../assets/alarm.mp3')
+
+var blink_value = true
+var data_audio = null
+var data_start = false
+
+var regex =  /[0-9][0-9]:[0-9][0-9]/;
+
 function init_values(){
     document.getElementById('time-value').value = '00:00'
+}
+
+function is_time_value(time_value){
+    return regex.test(time_value)
 }
 
 function add_seconds(signal, value = 30){
@@ -13,37 +28,45 @@ function add_seconds(signal, value = 30){
 }
 
 function convert_str2number(time_value){
-    values = time_value.split(":");
-    seconds = Number(values[1]) + Number(values[0]) * 60
+    seconds = 0
+    if(is_time_value(time_value)){
+        values = time_value.split(":");
+        seconds = Number(values[1]) + Number(values[0]) * 60
+    }
     return seconds
 }
 
 function convert_number2str(seconds){
-    minutes = ('0' + Math.floor(seconds/60)).slice(-2)
-    seconds = ('0' + (seconds - (60 * minutes))).slice(-2)
-    time_value = minutes + ':' + seconds
+    minutes = '0' + Math.floor(seconds/60)
+    seconds = '0' + (seconds - (60 * minutes))
+    time_value = minutes.slice(-2) + ':' + seconds.slice(-2)
     return time_value
 }
 
 function start_count(){
-    document.getElementById('time-value').data_start = true
-    if(document.getElementById('time-value').value !== '00:01' && document.getElementById('time-value').data_start == true){
-        add_seconds('-', 1)
-        setTimeout("start_count()", 1000)
+    time_value = document.getElementById('time-value').value
+    if(is_time_value(time_value)){
+        if(convert_str2number(document.getElementById('time-value').value) > 0){
+            t1 = setTimeout("start_count()", 1000)
+            add_seconds('-', 1)
+        }
+        else{
+            document.getElementById('time-value').value = '00:00'
+            if(t1){
+                clearTimeout(t1)
+                t1 = null
+                play_song()
+            }
+        }
     }
-    else{
-        add_seconds('-', 1)
-        play_song()
-        document.getElementById('time-value').data_start = false
-    }
+    else
+        document.getElementById('time-value').value = '00:00'
 }
 
 function play_song(){
-    if(!(document.getElementById('button-start').data_audio)){
-        audio = new Audio('../assets/alarm.mp3')
+    if(!(data_audio)){
         audio.play()
-        document.getElementById('button-start').data_audio = audio
-        document.getElementById('body').data_color = true
+        data_audio = audio
         blink_color(true)
     }
 }
@@ -52,24 +75,31 @@ function stop_song(){
     document.getElementById('body').style.backgroundColor = "black";
     document.getElementById('time-value').style.backgroundColor = "black"
     document.getElementById('time-value').value = '00:00'
-    audio = document.getElementById('button-start').data_audio
-    document.getElementById('button-start').data_audio = null
-    document.getElementById('body').data_color = null
-    document.getElementById('time-value').data_start = false
-    if(audio)
+
+    clearTimeout(t1)
+    t1 = null
+    clearTimeout(t2)
+    t2 = null
+
+    if(data_audio){
         audio.pause()
+
+        data_audio = null
+        blink_value = null
+        data_start = false
+    }
 
 }
 
-function blink_color(blink_value){
+function blink_color(blink_parameter){
     color = {true: 'red',
              false: 'black'}
-    if(blink_value != null){
-        document.getElementById('body').style.backgroundColor = color[blink_value]
-        document.getElementById('time-value').style.backgroundColor = color[blink_value]
-        document.getElementById('body').data_color = !blink_value
-        setTimeout("blink_color(blink_color(document.getElementById('body').data_color))", 500)
-    }
+
+    document.getElementById('body').style.backgroundColor = color[blink_parameter]
+    document.getElementById('time-value').style.backgroundColor = color[blink_parameter]
+    blink_value = !blink_parameter
+
+    t2 = setTimeout("blink_color(blink_value)", 500)
 }
 
 
